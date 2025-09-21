65e5671899ff8b4ee35906eb61d22a028ea36c65
Fenna Kort <fenna.kort@tutanota.com>
2025-09-21 00:11:22 -0600

    closes #2 - fileIn behaviour

--- src/app/AppDriver.java
+++ src/app/AppDriver.java
@@ -9,7 +9,7 @@ public class AppDriver {
     public static void main(String[] args) throws FileNotFoundException {
         
         Scanner kInput = new Scanner(System.in);
-        ArrayList<String> workerData = new ArrayList<String>();
+        //ArrayList<String> workerData = new ArrayList<String>();
 
         
         System.out.println("Please provide the name and full file path of the input file: ");
@@ -20,11 +20,13 @@ public class AppDriver {
         
         File workerFile = new File(inputFilePath);
         Scanner fInput = new Scanner(workerFile);
+        String headingsLine = new String(fInput.nextLine()); //separate out headings line to initialize correct line for while loop and store headings for fileOut
+        String[] headingsArray = headingsLine.split("\t");
+        
         while (fInput.hasNext()) {
             String applicantLine = new String(fInput.nextLine());
-            //System.out.println(applicantLine);
             Applicant currentApplicant = new Applicant(applicantLine);
-            currentApplicant.scoreLanguageSkills();
+            
             
             }
         
