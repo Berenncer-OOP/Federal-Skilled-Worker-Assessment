4e202edbe495dec0da96e0c2793b2ec831acea8f
Fenna Kort <fenna.kort@tutanota.com>
2025-09-20 19:12:52 -0600

    waiting on code from Bernard that will populate fields of applicant object with data from string applicantLine when creating an instance of Applicant.

--- src/app/Applicant.java
+++ src/app/Applicant.java
@@ -1,33 +1,62 @@
 package app;
 
+import java.util.Scanner;
+
 public class Applicant {
 //fields: all applicant data
 //first_name	last_name	age	marital_status	speak_1	listen_1	read_1	write_1	all_2	education	work_experience	arranged_employment	adaptability_spouse_language	adaptability_spouse_education	adaptability_spouse_work	adaptability_you_education	adaptability_you_work	adaptability_you_employment	adaptability_relatives
 
 	//AND score
-	private String first_name;
-	private String last_name;
-	private String age;
-	private boolean marital_status;
-	private int speak_1;
-	private int listen_1;
-	private int read_1;
-	private int write_1;
-	private boolean all_2; //min CLB 5 across prev. 4 language skills
-	private String education;
-	private int work_experience; //years
-	private boolean arranged_employment;
-	private boolean spouse_language;
-	private boolean spouse_education;
-	private boolean spouse_work;
-	private boolean adaptability_education;
-	private boolean adaptability_work;
-	private boolean adaptability_employment;
-	private boolean relatives;
-	private int score;
+	private String firstName;
+	private String lastName;
+	private int age;
+	private boolean maritalStatus;
 	
+	private int speak;
+	private int listen;
+	private int read;
+	private int write;
+	private boolean minimumCLB; //min CLB 5 across prev. 4 language skills
+	
+	private String education;
+	private int workExperience; //years
+	private boolean arrangedEmployment;
+	
+	private boolean spouseLanguage;
+	private boolean spouseEducation;
+	private boolean spouseWork;
+	
+	private boolean adaptabilityEducation;
+	private boolean adaptabilityWork;
+	private boolean adaptabilityEmployment;
+	private boolean relatives;
+	
+	private int applicantScore;
+
 	public Applicant(String applicantLine) {
 	    
+	}
+	public void fillData(int temp, String currToken) {
+	    switch(temp) {
+	    case 0:
+	        this.firstName = currToken;
+	        System.out.println(this.firstName);
+	        break;
+	    case 1:
+	    	this.lastName = currToken;
+	        break;
+	    case 2:
+	        this.age = Integer.parseInt(currToken);
+	    case 3:
+	    	this.maritalStatus = Integer.parseInt(currToken)
+	    }
+	}
+	public void addAppData(String tmpString) {
+	    Scanner strReader = new Scanner(tmpString);
+	    String currToken;
+	    for(int i = 0; i <= 18; i++) {
+	        fillData(i, strReader.next());
+	    }
 	}
 	
 //methods: ???
@@ -36,4 +65,73 @@ public class Applicant {
 	//getAge
 	//getScore
 	//setScore
+	
+	public int scoreLanguageSkills() {
+		int languageScore = 0;
+		if (this.speak == 7) {
+			languageScore += 4;
+		}
+		else if (this.speak == 8) {
+			languageScore += 5;
+		}
+		else if (this.speak >= 9) {
+			languageScore += 6;
+		}
+		else {
+			languageScore += 0;
+		}
+		
+		if (this.listen == 7) {
+			languageScore += 4;
+		}
+		else if (this.listen == 8) {
+			languageScore += 5;
+		}
+		else if (this.listen >= 9) {
+			languageScore += 6;
+		}
+		else {
+			languageScore += 0;
+		}
+		
+		if (this.read == 7) {
+			languageScore += 4;
+		}
+		else if (this.read == 8) {
+			languageScore += 5;
+		}
+		else if (this.read >= 9) {
+			languageScore += 6;
+		}
+		else {
+			languageScore += 0;
+		}
+		
+		if (this.write == 7) {
+			languageScore += 4;
+		}
+		else if (this.write == 8) {
+			languageScore += 5;
+		}
+		else if (this.write >= 9) {
+			languageScore += 6;
+		}
+		else {
+			languageScore += 0;
+		}
+		
+		//secondary language
+		
+		if (this.minimumCLB == true) {
+			languageScore += 4;
+		}
+		else {
+			languageScore += 0;
+		}
+		
+		
+		System.out.println(languageScore);
+		return languageScore;		
+	}
+	
 }
